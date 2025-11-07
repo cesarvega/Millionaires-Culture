40684ba9e2ab8f38a6bdf4305543de416149e270
Cesar Vega <cesarvega.col@gmail.com>
2025-11-07 10:15:52 -0500

    Initial Commit

---
+++ Millionaires Culture/Millionaires_CultureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Millionaires_CultureApp.swift
+//  Millionaires Culture
+//
+//  Created by Cesar Vega on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Millionaires_CultureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Millionaires CultureTests/Millionaires_CultureTests.swift
@@ -0,0 +1,17 @@
+//
+//  Millionaires_CultureTests.swift
+//  Millionaires CultureTests
+//
+//  Created by Cesar Vega on 11/7/25.
+//
+
+import Testing
+@testable import Millionaires_Culture
+
+struct Millionaires_CultureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
